6ef481c755ebf7b87cd67ba4bad83811bb156679
Rhiadi Haiek <rhiadihaiek@hotmail.com>
2023-05-18 22:24:06 -0300

    Configura Workflows

--- Dockerfile
+++ Dockerfile
@@ -5,16 +5,12 @@ FROM python:${PYTHON_VERSION}
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
-RUN mkdir -p /code
-
 WORKDIR /code
 
 RUN pip install pipenv
 COPY Pipfile Pipfile.lock /code/
 RUN pipenv install --deploy --system
 COPY . /code
-
-RUN python manage.py collectstatic --noinput
 
 EXPOSE 8000
 
--- pypro/settings.py
+++ pypro/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -120,7 +119,7 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / "public"
 STATIC_URL = "static/"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
