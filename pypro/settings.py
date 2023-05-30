90657ece780d4f375c5fc23e7ce4ebb0c7cc5a23
Rhiadi Haiek <rhiadihaiek@hotmail.com>
2023-05-30 19:44:50 -0300

    Configura AWS

--- pypro/settings.py
+++ pypro/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / "public"
 STATIC_URL = "static/"
 
+MEDIA_URL = "/media"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
